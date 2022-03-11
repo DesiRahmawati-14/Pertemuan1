176a5c85d6fdb59ac9de601fe09c1ca9bf4c18e1
FCM <rahmawatidesi730@gmail.com>
2022-03-11 11:31:05 +0700

    initial project pertemuan1

---
+++ app/src/main/java/com/example/newpro/MainApp.kt
@@ -0,0 +1,7 @@
+package com.example.newpro
+
+//fungsi main adalah fungsi yang pertama kali di eksekusi
+fun main() {
+    //untuk mencetak string ke layar
+    println("Hello, saya sudah baru belajar kotlin")
+}
